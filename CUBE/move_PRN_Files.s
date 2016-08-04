b4532cd305bdfa04abd0d5cae9c16701bdec1f29
Amar <sarvepalli@pbworld.com>
2016-08-04 02:24:00 -0400

    copy mdb template to output folder

--- CUBE/move_PRN_Files.s
+++ CUBE/move_PRN_Files.s
@@ -1,3 +1,6 @@
 ; Do not change filenames or add or remove FILEI/FILEO statements using an editor. Use Cube/Application Manager.
+*IF EXIST "{SCENARIO_DIR}\Output\Output.MDB" DEL "{SCENARIO_DIR}\Output\Output.MDB"
+*COPY "{SCENARIO_DIR}\MAPS\EMPTY.MDB" "{SCENARIO_DIR}\OUTPUT\OUTPUT.MDB"
 *COPY "{CATALOG_DIR}\CUBE\*.PRN" "{SCENARIO_DIR}\OUTPUT\LOGS\*.PRN"
 *DEL "{CATALOG_DIR}\CUBE\*.PRN" 
+c
